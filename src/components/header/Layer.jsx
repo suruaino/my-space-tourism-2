1fa5303507ece355843fc18971d16df45fa9538d
SURU EMMANUEL <suruaino@gmail.com>
2024-05-22 20:44:12 +0100

    fixed home

--- src/components/header/Layer.jsx
+++ src/components/header/Layer.jsx
@@ -1,17 +1,31 @@
+import React, { useState } from 'react';
 import logo from "./header-assets/logo.svg";
+import menu from "./header-assets/menu.png";
+import close from "./header-assets/close.png";
 import "./header.css";
 import { Link, Outlet } from "react-router-dom";
 
 const Layer = () => {
+
+  const [showNav, setShoNav] = useState(false);
+
+  const menuToggle = (index) => {
+    setShoNav(true);
+  };
   return (
     <div className="layer">
       <header>
-        <div className="logo"><img src={logo} alt="" /></div>
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
 
         <nav>
+    
           <ul>
+
             <li>
               <Link to="/Home">
+              {/* <input type="checkbox" /> */}
                 <span>00</span> Home
               </Link>
             </li>
@@ -30,9 +44,12 @@ const Layer = () => {
               <Link to="/Technology">
                 <span>03</span> Technology
               </Link>
-            </li>
-          </ul>
+            </li> 
+           </ul>
+
         </nav>
+        <img className='menuBtn' src={menu} alt="" />
+        <img className='closeBtn' src={close} alt="" />
       </header>
       <Outlet />
     </div>
